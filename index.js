d4210dbdf87f99a8402f4fe243e0f1d48f93fb32
Jesper Bengtsson Ekwall <jeppen@live.com>
2019-10-31 13:07:37 +0100

    Changed to uniform quotes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // QuerySelect av huvudelementen
-let main = document.querySelector('main');
+let main = document.querySelector("main");
 
 
 // Länkar till main.css
-let cssMainLink = document.createElement('link');
+let cssMainLink = document.createElement("link");
 cssMainLink.rel = "stylesheet";
 cssMainLink.type = "text/css";
 cssMainLink.href = "main.css";
@@ -15,11 +15,11 @@ document.title = "Fruits & Vegetables Corp";
 
 
 // Ändrar Överskriften till rätt företagsnamn
-document.querySelector('h1').textContent = "Fruits & Vegetables Corp";
+document.querySelector("h1").textContent = "Fruits & Vegetables Corp";
 
 
 // Ändrar sista länken i listan samt texten i länken i listan till Vegetables
-let a = document.querySelector('ul li:last-child a');
+let a = document.querySelector("ul li:last-child a");
 a.textContent = "Vegetables";
 a.href = "#vegetables";
 
@@ -27,7 +27,7 @@ a.href = "#vegetables";
 // Lägger till texten i en p-tagg samt lägger flyttar "About" ovanför "Contact"
 let about = document.querySelector("#about");
 let aboutH2 = document.createElement("h2");
-let aboutP = document.createElement('p');
+let aboutP = document.createElement("p");
 
 about.innerHTML = "";
 aboutH2.textContent = "About";
@@ -39,23 +39,23 @@ main.insertBefore(about, main.firstChild);
 
 
 // Lägger till h2 i "Contact" samt sätter texten till "Contact"
-let contact = document.querySelector('#contact');
-let h2Contact = document.createElement('h2');
+let contact = document.querySelector("#contact");
+let h2Contact = document.createElement("h2");
 h2Contact.textContent = "Contact";
 contact.appendChild(h2Contact);
 contact.insertBefore(h2Contact, contact.firstChild);
 
 
 // Tar bort befintlig tr i thead
-let thead = document.querySelector('thead');
-let tr = document.querySelector('thead tr');
+let thead = document.querySelector("thead");
+let tr = document.querySelector("thead tr");
 thead.removeChild(tr);
 
 
 // Lägger till tr samt th i thead samt sätter texten i th
-let newTr = document.createElement('tr');
-let newTh1 = document.createElement('th');
-let newTh2 = document.createElement('th');
+let newTr = document.createElement("tr");
+let newTh1 = document.createElement("th");
+let newTh2 = document.createElement("th");
 
 newTh1.textContent = "Name";
 newTh2.textContent = "Email";
